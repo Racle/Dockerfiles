5dbe2239be02a93c4ac9bb9c21a34acb258b7847
vvi <vvi@festum.fi>
2018-04-04 17:22:58 +0300

    fixed typo. Updated .desktop file

--- redis-desktop-manager/install.sh
+++ redis-desktop-manager/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p /usr/share/redis-desktop-manager/
 rm /usr/share/redis-desktop-manager/rdm.png
-rm /usr/share/redis-desktop-manager/rdm.png
+rm /usr/share/redis-desktop-manager/rdm.sh
 rm /usr/share/applications/racle-rdm.desktop
 wget https://raw.githubusercontent.com/uglide/RedisDesktopManager/0.9/src/resources/rdm.png -P /usr/share/redis-desktop-manager/
 wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.sh -P /usr/share/redis-desktop-manager/
