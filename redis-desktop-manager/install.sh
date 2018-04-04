43f8740fccf69fe40384b11802539ca43f30a97f
Ville Viitaharju <villeviitaharju@gmail.com>
2018-04-04 19:04:13 +0300

    Update install.sh

--- redis-desktop-manager/install.sh
+++ redis-desktop-manager/install.sh
@@ -8,7 +8,8 @@ rm /usr/share/applications/racle-rdm.desktop
 echo Getting new files..
 wget https://raw.githubusercontent.com/uglide/RedisDesktopManager/0.9/src/resources/rdm.png -P /usr/share/redis-desktop-manager/ > /dev/null 2>&1
 wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.sh -P /usr/share/redis-desktop-manager/ > /dev/null 2>&1
-wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications/ > /dev/null 2>&1
+#wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications/ > /dev/null 2>&1
+sh -c "curl https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop > /usr/share/applications/racle-rdm.desktop"
 chmod a+x /usr/share/applications/racle-rdm.desktop
 chmod a+x /usr/share/redis-desktop-manager/rdm.sh
 ln -s /usr/share/redis-desktop-manager/rdm.sh /usr/local/bin/rdm
