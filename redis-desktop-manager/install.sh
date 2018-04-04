65d86e6ed955343079ce6c694fa2a914929e775e
Ville Viitaharju <villeviitaharju@gmail.com>
2018-04-04 19:18:32 +0300

    Update install.sh

--- redis-desktop-manager/install.sh
+++ redis-desktop-manager/install.sh
@@ -8,10 +8,11 @@ rm /usr/share/applications/racle-rdm.desktop
 echo Getting new files..
 wget https://raw.githubusercontent.com/uglide/RedisDesktopManager/0.9/src/resources/rdm.png -P /usr/share/redis-desktop-manager/ > /dev/null 2>&1
 wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.sh -P /usr/share/redis-desktop-manager/ > /dev/null 2>&1
-#wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications/ > /dev/null 2>&1
-sh -c "curl https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop > /usr/share/applications/racle-rdm.desktop"
+wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications/ > /dev/null 2>&1
 chmod a+x /usr/share/applications/racle-rdm.desktop
 chmod a+x /usr/share/redis-desktop-manager/rdm.sh
 ln -s /usr/share/redis-desktop-manager/rdm.sh /usr/local/bin/rdm
+#fixing .destop icon bug
+sed -i 's/\r//' /usr/share/applications/racle-rdm.desktop
 echo Pulling latest docker image...
 docker pull racle90/redis-desktop-manager
