a27b541427bb0526360a593e629dd3e998a3f892
vvi <vvi@festum.fi>
2018-04-04 17:15:29 +0300

    updated install script

--- redis-desktop-manager/install.sh
+++ redis-desktop-manager/install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-sudo mkdir -p /usr/share/redis-desktop-manager/
-sudo wget https://raw.githubusercontent.com/uglide/RedisDesktopManager/0.9/src/resources/rdm.png -P /usr/share/redis-desktop-manager/
-sudo wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.sh -P /usr/share/redis-desktop-manager/
-sudo wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications
-sudo chmod a+x /usr/share/applications/racle-rdm.desktop
-sudo chmod a+x /usr/share/redis-desktop-manager/rdm.sh
+mkdir -p /usr/share/redis-desktop-manager/
+rm /usr/share/redis-desktop-manager/rdm.png
+rm /usr/share/redis-desktop-manager/rdm.png
+rm /usr/share/applications/racle-rdm.desktop
+wget https://raw.githubusercontent.com/uglide/RedisDesktopManager/0.9/src/resources/rdm.png -P /usr/share/redis-desktop-manager/
+wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.sh -P /usr/share/redis-desktop-manager/
+wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications
+chmod a+x /usr/share/applications/racle-rdm.desktop
+chmod a+x /usr/share/redis-desktop-manager/rdm.sh
