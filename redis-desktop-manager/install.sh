d6ffaec45efdc13478f1eba1045c98d5d30192db
Ville Viitaharju <villeviitaharju@gmail.com>
2018-04-05 07:36:06 +0300

    Update install.sh

--- redis-desktop-manager/install.sh
+++ redis-desktop-manager/install.sh
@@ -4,12 +4,12 @@ mkdir -p /usr/share/redis-desktop-manager/
 echo Removing old files..
 rm /usr/share/redis-desktop-manager/rdm.png
 rm /usr/share/redis-desktop-manager/rdm.sh
-rm /usr/share/applications/racle-rdm.desktop
+rm /usr/share/applications/rdm.desktop
 rm /usr/local/bin/rdm
 echo Getting new files..
 wget https://raw.githubusercontent.com/uglide/RedisDesktopManager/0.9/src/resources/rdm.png -P /usr/share/redis-desktop-manager/ > /dev/null 2>&1
 wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.sh -P /usr/share/redis-desktop-manager/ > /dev/null 2>&1
-wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/racle-rdm.desktop -P /usr/share/applications/ > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Racle/Dockerfiles/master/redis-desktop-manager/rdm.desktop -P /usr/share/applications/ > /dev/null 2>&1
 chmod a+x /usr/share/applications/racle-rdm.desktop
 chmod a+x /usr/share/redis-desktop-manager/rdm.sh
 ln -s /usr/share/redis-desktop-manager/rdm.sh /usr/local/bin/rdm
